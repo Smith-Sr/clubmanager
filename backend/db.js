c7e9c2da357538d7a047239274f53f3fc5dfbb2c
SMITH LIMAS <smithagosto08@gmail.com>
2026-05-06 19:05:51 -0500

    feat: implementación archivo db.js con ajustes de conección a la base de datos

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host:     'localhost',
+  user:     'root',
+  password: '',        // tu password de MySQL
+  database: 'clubmanager'
+});
+
+db.connect(err => {
+  if (err) console.error('Error DB:', err);
+  else console.log('MySQL conectado');
+});
+
+module.exports = db;
